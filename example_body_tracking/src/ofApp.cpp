ed24590a8d86bbabdbaef5feeb1eda7dba39b272
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-15 19:19:33 +0900

    feat: add meaningful 2d example for body tracking

--- example_body_tracking/src/ofApp.cpp
+++ example_body_tracking/src/ofApp.cpp
@@ -21,23 +21,32 @@ void ofApp::update(){
 void ofApp::draw(){
     video.draw(0, 0);
 
-    auto keypoints = maxine.get_keypoints3D();
+    // auto keypoints3d = maxine.get_keypoints3D();
 
-    for(size_t i=0; i<keypoints.size(); ++i){
-        ofVec3f vec = keypoints.at(i);
-        for(size_t j=0; j<3; ++j){
-            float v = vec.x;
-            if(j==1){
-                v = vec.y;
-            }else if(j==2){
-                v = vec.z;
-            }
+    // for(size_t i=0; i<keypoints3d.size(); ++i){
+    //     ofVec3f vec = keypoints3d.at(i);
+    //     for(size_t j=0; j<3; ++j){
+    //         float v = vec.x;
+    //         if(j==1){
+    //             v = vec.y;
+    //         }else if(j==2){
+    //             v = vec.z;
+    //         }
 
-            ofPushStyle();
-            ofSetColor(ofMap(v, -10, 10, 0, 255, true), 0, 0);
-            ofDrawRectangle(i * 10, 500 + 10 * j, 10, 10);
-            ofPopStyle();
-        }
+    //         ofPushStyle();
+    //         ofSetColor(ofMap(v, -10, 10, 0, 255, true), 0, 0);
+    //         ofDrawRectangle(i * 10, 500 + 10 * j, 10, 10);
+    //         ofPopStyle();
+    //     }
+    // }
+
+    auto keypoints2d = maxine.get_keypoints();
+    for(size_t i=0; i<keypoints2d.size(); ++i){
+        ofVec2f v = keypoints2d.at(i);
+        ofPushStyle();
+        ofSetColor(255, 0, 0);
+        ofDrawRectangle(v.x - 5, v.y - 5, 10, 10);
+        ofPopStyle();
     }
 
 
