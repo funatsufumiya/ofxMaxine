2b3e05a6897a55bbdafa307289c2d0ad152f6058
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-11-15 18:51:02 +0900

    feat: add value tests for body tracking

--- example_body_tracking/src/ofApp.cpp
+++ example_body_tracking/src/ofApp.cpp
@@ -21,6 +21,26 @@ void ofApp::update(){
 void ofApp::draw(){
     video.draw(0, 0);
 
+    auto keypoints = maxine.get_keypoints3D();
+
+    for(size_t i=0; i<keypoints.size(); ++i){
+        ofVec3f vec = keypoints.at(i);
+        for(size_t j=0; j<3; ++j){
+            float v = vec.x;
+            if(j==1){
+                v = vec.y;
+            }else if(j==2){
+                v = vec.z;
+            }
+
+            ofPushStyle();
+            ofSetColor(ofMap(v, -10, 10, 0, 255, true), 0, 0);
+            ofDrawRectangle(i * 10, 500 + 10 * j, 10, 10);
+            ofPopStyle();
+        }
+    }
+
+
     ofDrawBitmapStringHighlight(ofToString(ofGetFrameRate(), 2, 0, '\0'), 20, 20);
 }
 
